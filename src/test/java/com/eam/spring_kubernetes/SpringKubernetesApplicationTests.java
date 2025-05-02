97420babf69fdb8d12c71468db5f0fad3027b94e
Danysoftdev <danysoftdev@gmail.com>
2025-05-02 11:47:23 -0500

    first commit

---
+++ src/test/java/com/eam/spring_kubernetes/SpringKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.spring_kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
